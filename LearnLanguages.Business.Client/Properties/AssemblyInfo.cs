0357edc62d73e260302ba3d9806e3a45ec1de15e
William Raiford <bill.mybiz@gmail.com>
2012-01-06 21:20:16 -0600

    Recreated Business.Client to be SL class library, not application.
    I didn't like having the xap file in the Host, I just have a habit of
    not using SL class libraries from when I first learned MEF.
    
    Signed-off-by: William Raiford <bill.mybiz@gmail.com>

--- LearnLanguages.Business.Client/Properties/AssemblyInfo.cs
+++ LearnLanguages.Business.Client/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cad30bcf-ff7f-417c-aad9-da7f170894df")]
+[assembly: Guid("54be2f61-0ffa-4d35-8b0a-d7075da82a90")]
 
 // Version information for an assembly consists of the following four values:
 //
