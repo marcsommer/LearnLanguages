ddf4f7804c79632219dcd5e8ea39ba685e6ad2e5
William Raiford <bill.mybiz@gmail.com>
2012-08-21 13:28:08 -0500

    Can edit Answer somewhat now as well as Question.
    -Having problem with duplicating and not saving phrase properly similar
    to when doing the question.
    -Need to change this stuff to another view model whose responsibility
    is to simply contain a PhraseEdit and do this editing.  If the translation
    object is in memory (or the LineEdit for that matter), these will need
    to know that a phrase they contain has been updated.
    
    Signed-off-by: William Raiford <bill.mybiz@gmail.com>

--- LearnLanguages.Study.Client/ViewModels/StudyPhraseManualQuestionAnswerViewModel.cs
+++ LearnLanguages.Study.Client/ViewModels/StudyPhraseManualQuestionAnswerViewModel.cs
@@ -145,8 +145,6 @@ namespace LearnLanguages.Study.ViewModels
     }
     public string ShowAnswerButtonLabel { get { return StudyResources.ButtonLabelShowAnswer; } }
 
-    private string _InitialQuestionText;
-    private string _ModifiedQuestionText;
     #endregion
 
     #region Methods
@@ -156,6 +154,8 @@ namespace LearnLanguages.Study.ViewModels
       Question = question;
       _InitialQuestionText = question.Text;
       _ModifiedQuestionText = "";
+      _InitialAnswerText = answer.Text;
+      _ModifiedAnswerText = "";
       Answer = answer;
       HideAnswer();
       callback(null);
@@ -221,6 +221,7 @@ namespace LearnLanguages.Study.ViewModels
     public void ShowAnswer()
     {
       AnswerVisibility = Visibility.Visible;
+      UpdateEditAnswerButtonVisibilities();
       HidingAnswer = false;
 
       _DateTimeAnswerShown = DateTime.Now;
@@ -261,6 +262,8 @@ namespace LearnLanguages.Study.ViewModels
     }
 
     #region Edit/Save Question Stuff
+
+    #region Question
 
     private Visibility _EditQuestionButtonVisibility = Visibility.Visible;
     public Visibility EditQuestionButtonVisibility
@@ -320,7 +323,7 @@ namespace LearnLanguages.Study.ViewModels
     {
       QuestionIsReadOnly = false;
       //hack: UpdateEditButtonVisibilities method instead of visibility converter
-      UpdateEditButtonVisibilities();
+      UpdateEditQuestionButtonVisibilities();
     }
 
     public bool CanSaveQuestion
@@ -389,7 +392,7 @@ namespace LearnLanguages.Study.ViewModels
                 //FINISH UP
                 SaveQuestionButtonIsEnabled = true;
                 QuestionIsReadOnly = true;
-                UpdateEditButtonVisibilities();
+                UpdateEditQuestionButtonVisibilities();
                 _InitialQuestionText = _ModifiedQuestionText;
                 _ModifiedQuestionText = "";
               });
@@ -405,7 +408,7 @@ namespace LearnLanguages.Study.ViewModels
             Question = (PhraseEdit)r.NewObject;
             SaveQuestionButtonIsEnabled = true;
             QuestionIsReadOnly = true;
-            UpdateEditButtonVisibilities();
+            UpdateEditQuestionButtonVisibilities();
           });
       }
     }
@@ -425,7 +428,7 @@ namespace LearnLanguages.Study.ViewModels
       }
     }
 
-    private void UpdateEditButtonVisibilities()
+    private void UpdateEditQuestionButtonVisibilities()
     {
       if (QuestionIsReadOnly)
       {
@@ -438,6 +441,203 @@ namespace LearnLanguages.Study.ViewModels
         SaveQuestionButtonVisibility = Visibility.Visible;
       }
     }
+
+    private string _InitialQuestionText;
+    private string _ModifiedQuestionText;
+
+    #endregion
+
+    #region Answer
+
+    private Visibility _EditAnswerButtonVisibility = Visibility.Collapsed;
+    public Visibility EditAnswerButtonVisibility
+    {
+      get { return _EditAnswerButtonVisibility; }
+      set
+      {
+        if (value != _EditAnswerButtonVisibility)
+        {
+          _EditAnswerButtonVisibility = value;
+          NotifyOfPropertyChange(() => EditAnswerButtonVisibility);
+        }
+      }
+    }
+
+    private Visibility _SaveAnswerButtonVisibility = Visibility.Collapsed;
+    public Visibility SaveAnswerButtonVisibility
+    {
+      get { return _SaveAnswerButtonVisibility; }
+      set
+      {
+        if (value != _SaveAnswerButtonVisibility)
+        {
+          _SaveAnswerButtonVisibility = value;
+          NotifyOfPropertyChange(() => SaveAnswerButtonVisibility);
+        }
+      }
+    }
+
+    public string ButtonLabelEditAnswer { get { return StudyResources.ButtonLabelEdit; } }
+    public string ButtonLabelSaveAnswer { get { return StudyResources.ButtonLabelSave; } }
+
+    private bool _AnswerIsReadOnly = true;
+    public bool AnswerIsReadOnly
+    {
+      get { return _AnswerIsReadOnly; }
+      set
+      {
+        if (value != _AnswerIsReadOnly)
+        {
+          _AnswerIsReadOnly = value;
+          NotifyOfPropertyChange(() => AnswerIsReadOnly);
+        }
+      }
+    }
+
+    public bool CanEditAnswer
+    {
+      get
+      {
+        //if the question is read only, then we are NOT currently editing the question.
+        //therefore, we can press the edit question button which is what CanEditAnswer is asking.
+        return AnswerIsReadOnly;
+      }
+    }
+    public void EditAnswer()
+    {
+      AnswerIsReadOnly = false;
+      //hack: UpdateEditButtonVisibilities method instead of visibility converter
+      UpdateEditAnswerButtonVisibilities();
+    }
+
+    public bool CanSaveAnswer
+    {
+      get
+      {
+        //return Answer != null && Answer.IsSavable && SaveAnswerButtonIsEnabled;
+        return Answer != null && Answer.IsValid && SaveAnswerButtonIsEnabled;
+      }
+    }
+    public void SaveAnswer()
+    {
+      AnswerIsReadOnly = true;
+      SaveAnswerButtonIsEnabled = false;
+
+      if (Answer.IsChild)
+      {
+        //QUESTION IS CHILD, AND I'M NOT ABLE TO GET THE PARENT SAVE TO WORK, SO
+        //I'M GOING TO GET THE QUESTION FROM THE DB DIRECTLY, SAVE IT WITH THE NEW TEXT,
+        //AND REPLACE THE QUESTION WITH THE SAVED DB NON-CHILD OBJECT.
+
+        //I'M GOING TO HACK THIS UP NOW CUZ I NEED TO GET MOVING.
+        //MAKING TWO FIELDS FOR THIS VM: INITIAL AND MODIFIED QUESTION TEXT.
+        //GOING TO USE THESE TO MANIPULATE SHIT HOW I WANT IT
+
+        //The Answer PhraseEdit has the modified text, but we need to search
+        //via the initial text so we're going to store the modified text,
+        //re-apply the initial text, do the search, pull the old DB object,
+        //modify THAT PhraseEdit with the stored modified text, save it,
+        //then store the newly saved (with new text) PhraseEdit as our Answer.
+
+        _ModifiedAnswerText = Answer.Text;
+        Answer.Text = _InitialAnswerText;
+        var criteria = new Business.Criteria.ListOfPhrasesCriteria(Answer);
+        PhrasesByTextAndLanguageRetriever.CreateNew(criteria, (s, r) =>
+        //PhraseEdit.GetPhraseEdit(Answer.Id, (s, r) =>
+          {
+            if (r.Error != null)
+              throw r.Error;
+            //var retrievedAnswer = r.Object;
+
+            var retrievedAnswer = r.Object.RetrievedPhrases[Answer.Id];
+            if (retrievedAnswer == null)
+              throw new Exception("retrieved question is null");
+
+            retrievedAnswer.Text = _ModifiedAnswerText;
+
+            //RETRIEVED QUESTION IS NOW EDITED, NEED TO SAVE IT
+            retrievedAnswer.BeginSave((s2, r2) =>
+              {
+                if (r2.Error != null)
+                  throw r2.Error;
+                Answer = (PhraseEdit)r2.NewObject;
+
+                //debug
+                //confirm save
+                var crit = new Business.Criteria.ListOfPhrasesCriteria(Answer);
+                PhrasesByTextAndLanguageRetriever.CreateNew(crit, (ss, rr) =>
+                  {
+                    if (rr.Error != null)
+                      throw rr.Error;
+
+                  });
+                //end debug
+
+                //FINISH UP
+                SaveAnswerButtonIsEnabled = true;
+                AnswerIsReadOnly = true;
+                UpdateEditAnswerButtonVisibilities();
+                _InitialAnswerText = _ModifiedAnswerText;
+                _ModifiedAnswerText = "";
+              });
+          });
+      }
+      else
+      {
+        Answer.BeginSave((s, r) =>
+          {
+            if (r.Error != null)
+              throw r.Error;
+
+            Answer = (PhraseEdit)r.NewObject;
+            SaveAnswerButtonIsEnabled = true;
+            AnswerIsReadOnly = true;
+            UpdateEditAnswerButtonVisibilities();
+          });
+      }
+    }
+    
+    private bool _SaveAnswerButtonIsEnabled = true;
+    public bool SaveAnswerButtonIsEnabled
+    {
+      get { return _SaveAnswerButtonIsEnabled; }
+      set
+      {
+        if (value != _SaveAnswerButtonIsEnabled)
+        {
+          _SaveAnswerButtonIsEnabled = value;
+          NotifyOfPropertyChange(() => SaveAnswerButtonIsEnabled);
+          NotifyOfPropertyChange(() => CanSaveAnswer);
+        }
+      }
+    }
+
+    private void UpdateEditAnswerButtonVisibilities()
+    {
+      //IF THE ANSWER ISN'T SHOWN YET, WE DON'T WANT TO SEE EDIT/SAVE BUTTONS EITHER
+      if (AnswerVisibility == Visibility.Collapsed)
+      {
+        EditAnswerButtonVisibility = Visibility.Collapsed;
+        SaveAnswerButtonVisibility = Visibility.Collapsed;
+      }
+      //IF THE ANSWER BUTTON IS SHOWN AND ANSWER IS READONLY, THEN WE CAN EDIT BUT NOT SAVE
+      else if (AnswerIsReadOnly)
+      {
+        EditAnswerButtonVisibility = Visibility.Visible;
+        SaveAnswerButtonVisibility = Visibility.Collapsed;
+      }
+      //OTHERWISE, WE CAN SAVE BUT NOT EDIT
+      else
+      {
+        EditAnswerButtonVisibility = Visibility.Collapsed;
+        SaveAnswerButtonVisibility = Visibility.Visible;
+      }
+    }
+
+    private string _InitialAnswerText;
+    private string _ModifiedAnswerText;
+
+    #endregion
 
     #endregion
   }
