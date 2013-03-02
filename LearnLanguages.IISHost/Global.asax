eda379da7bf5a44b65df4f1daff1b63c81e5544e
William Raiford <bill.mybiz@gmail.com>
2013-03-02 17:11:55 -0600

    Deleted other host projects. Trying to do themes, but didn't work.
    -Overall, I'm failing. Need to do better.
    
    Signed-off-by: William Raiford <bill.mybiz@gmail.com>

--- LearnLanguages.IISHost/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearnLanguages.IISHost.Global" Language="C#" %>
---
+++ LearnLanguages.Mvc4/Global.asax.cs
@@ -0,0 +1,48 @@
+﻿using LearnLanguages.Business.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+
+namespace LearnLanguages.Mvc4
+{
+  // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+  // visit http://go.microsoft.com/?LinkId=9394801
+
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
+    {
+      ModelBinders.Binders.DefaultBinder = new Csla.Web.Mvc.CslaModelBinder();
+
+      AreaRegistration.RegisterAllAreas();
+
+      WebApiConfig.Register(GlobalConfiguration.Configuration);
+
+      //These filters set up an "implicit deny" type of structure. 
+      //Now, for any anonymous access, you have to add [AllowAnonymous]
+      GlobalFilters.Filters.Add(new HandleErrorAttribute());
+      GlobalFilters.Filters.Add(new System.Web.Mvc.AuthorizeAttribute());
+      FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+      BundleConfig.RegisterBundles(BundleTable.Bundles);
+      //BundleMobileConfig.RegisterBundles(BundleTable.Bundles);
+    }
+
+    protected void Application_AuthenticateRequest(Object sender, EventArgs e)
+    {
+      if (Mvc4Helper.CurrentUserIsAuthenticated())
+      {
+        UserPrincipal.Load(Csla.ApplicationContext.User.Identity.Name);
+      }
+    }
+
+
+  }
+}
