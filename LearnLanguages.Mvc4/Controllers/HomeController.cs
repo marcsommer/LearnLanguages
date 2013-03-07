57df13f42493cd2a8199d4146df656f67deb1dd6
William Raiford <bill.mybiz@gmail.com>
2013-03-06 22:28:45 -0600

    InProgress: Content and Display of web page.
    Signed-off-by: William Raiford <bill.mybiz@gmail.com>

--- LearnLanguages.Mvc4/Controllers/HomeController.cs
+++ LearnLanguages.Mvc4/Controllers/HomeController.cs
@@ -6,15 +6,18 @@ using System.Web.Mvc;
 
 namespace LearnLanguages.Mvc4.Controllers
 {
+  [AllowAnonymous]
   public class HomeController : Controller
   {
+    [AllowAnonymous]
     public ActionResult Index()
     {
-      ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+      //ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
       return View();
     }
 
+    [AllowAnonymous]
     public ActionResult About()
     {
       ViewBag.Message = "Your app description page.";
@@ -22,6 +25,7 @@ namespace LearnLanguages.Mvc4.Controllers
       return View();
     }
 
+    [AllowAnonymous]
     public ActionResult Contact()
     {
       ViewBag.Message = "Your contact page.";
